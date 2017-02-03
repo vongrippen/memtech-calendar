0bb4e63593b356b1b2297001e77836c148b8a88b
Mike Cochran <mcochran@linux.com>
2017-02-03 09:45:33 -0600

    Minify

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path')
+const webpack = require('webpack')
 
 module.exports = {
   context: __dirname,
@@ -9,6 +10,25 @@ module.exports = {
     publicPath: '/public/',
     filename: 'bundle.js'
   },
+  plugins: [
+    new webpack.optimize.UglifyJsPlugin({
+      compress: {
+        warnings: false,
+        screw_ie8: true,
+        conditionals: true,
+        unused: true,
+        comparisons: true,
+        sequences: true,
+        dead_code: true,
+        evaluate: true,
+        if_return: true,
+        join_vars: true
+      },
+      output: {
+        comments: false
+      }
+    })
+  ],
   resolve: {
     // alias: {
     //   react: 'preact-compat',
